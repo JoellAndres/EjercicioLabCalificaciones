75c8c4bec464e49dab1d7d1a8bcd16c775dddc4a
Franco Veloci <fveloci@hotmail.com>
2020-08-31 20:41:29 -0300

    try catch

--- calificaciones/Program.cs
+++ calificaciones/Program.cs
@@ -15,24 +15,37 @@ namespace calificaciones
             float suma = 0.0f, promedio = 0.0f;
             float nMay, nMen;
 
-            Console.Write("Ingrese la cantidad de alumnos: ");
-            nAlumnos = Convert.ToInt32(Console.ReadLine());
+            try{
+                Console.Write("Ingrese la cantidad de alumnos: ");
+                nAlumnos = Convert.ToInt32(Console.ReadLine());
+
+            }catch(Exception e){
+                Console.WriteLine("Ha ocurrido una excepcion: "+ e);
+            }
+            
 
             float[] calificaciones = new float[nAlumnos];
 
             //Pidiendo calificaciones
-            for(int i=0; i<nAlumnos; i++)
+            try
             {
-                Console.Write("Ingrese la calificación: ");
-                calificaciones[i] = Convert.ToSingle(Console.ReadLine());
-
-                while((calificaciones[i] < inferior) || (calificaciones[i] > superior))
+                for(int i=0; i<nAlumnos; i++)
                 {
-                    Console.WriteLine("\nLa calificacion debe estar entre (0 - 10)");
                     Console.Write("Ingrese la calificación: ");
                     calificaciones[i] = Convert.ToSingle(Console.ReadLine());
+
+                    while((calificaciones[i] < inferior) || (calificaciones[i] > superior))
+                    {
+                        Console.WriteLine("\nLa calificacion debe estar entre (0 - 10)");
+                        Console.Write("Ingrese la calificación: ");
+                        calificaciones[i] = Convert.ToSingle(Console.ReadLine());
+                    }
                 }
+
+            }catch(Exception e){
+                Console.WriteLine(e);
             }
+            
 
             //Obteniendo el promedio
             for(int i=0; i<nAlumnos; i++)
@@ -56,9 +69,9 @@ namespace calificaciones
 
             //Mostrando datos
             Console.Clear();
-            Console.WriteLine("El promedio es: " + promedio);
-            Console.WriteLine("La nota mayor es: " + nMay);
-            Console.WriteLine("La menor notas es: " + nMen);
+            Console.WriteLine("El promedio es: " + promedio + "\n");
+            Console.WriteLine("La nota mayor es: " + nMay + "\n");
+            Console.WriteLine("La nota menor es: " + nMen + "\n");
             Console.ReadKey();
         }
     }
